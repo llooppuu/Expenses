75b4fc456fa79ac7d9dc22a453240164235c4fd9
Lauri <lauri.vosmi@voco.ee>
2026-01-09 22:37:33 +0200

    Ülesanne 1 - kujunduse rakendamine komponendile

--- Expenses/src/components/ExpenseItem.jsx
+++ Expenses/src/components/ExpenseItem.jsx
@@ -3,10 +3,6 @@ import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
 
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    const month = props.date.toLocaleString('en-US', { month: 'long' });
-    const year = props.date.getFullYear();
-
     return (
     <div className='expense-item'>
         <ExpenseDate date={props.date}/>
