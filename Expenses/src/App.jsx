6247a957670aa94ea93529c6dece52c5609dee7f
Lauri <lauri.vosmi@voco.ee>
2026-01-09 21:43:43 +0200

    3. Komponendi sisustamine andmetega

--- Expenses/src/App.jsx
+++ Expenses/src/App.jsx
@@ -3,13 +3,30 @@ import './App.css'
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = {
+    date: new Date(2024, 10, 12),
+    title: 'New book',
+    price: 30.99
+  };
+
+  const expenses = [
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New book',
+      price: 30.99
+    },
+    {
+      date: new Date(2024, 9, 5),
+      title: 'New jeans',
+      price: 99.99
+    },
+  ];
 
   return (
-    <>
-    <ExpenseItem />
-    <ExpenseItem />
-    </>
+    <div className="App">
+      <ExpenseItem {...expenses[0]}/>
+      <ExpenseItem {...expenses[1]}/>
+    </div>
   )
 }
 
